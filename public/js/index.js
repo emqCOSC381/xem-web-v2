a86157bfa02fc17fb0d1e1fd2c17a8f9739cffd0
Qarani-m <emqarani@gmail.com>
2023-03-23 12:59:48 +0300

    new commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
 document.getElementById("searchBtn").addEventListener("mouseover",()=>{
     document.getElementById("searchBar").style.visibility="visible"
-
 })
+
---
+++ test/file.py
@@ -0,0 +1,12 @@
+import time 
+
+appologies = range(100)
+
+for appology in appologies:
+    time.sleep(0.5)
+    print(f"{appology}: man u wamepigwa seven nill 😂")
+
+
+
+
+    # angalia hapo chini
